69817ad7b4724ac8de975af7a0290e5691e198b5
Dylan Salas <adylansd28@gmail.com>
2025-07-17 12:39:12 -0500

    Completed and tested W03 assignment with all 5 problems passed

--- week03/code/Maze.cs
+++ week03/code/Maze.cs
@@ -5,63 +5,87 @@
 ///
 /// (x,y) : [left, right, up, down]
 ///
-/// 'x' and 'y' are integers and represents locations in the maze.
-/// 'left', 'right', 'up', and 'down' are boolean are represent valid directions
+/// 'x' and 'y' are integers and represent locations in the maze.
+/// 'left', 'right', 'up', and 'down' are booleans representing valid directions
 ///
-/// If a direction is false, then we can assume there is a wall in that direction.
-/// If a direction is true, then we can proceed.  
-///
-/// If there is a wall, then throw an InvalidOperationException with the message "Can't go that way!".  If there is no wall,
-/// then the 'currX' and 'currY' values should be changed.
+/// If a direction is false, we assume there is a wall in that direction.
+/// If a direction is true, movement is allowed.
+/// If there is a wall, throw InvalidOperationException with the message "Can't go that way!".
 /// </summary>
 public class Maze
 {
-    private readonly Dictionary<ValueTuple<int, int>, bool[]> _mazeMap;
+    private readonly Dictionary<(int, int), bool[]> _mazeMap;
     private int _currX = 1;
     private int _currY = 1;
 
-    public Maze(Dictionary<ValueTuple<int, int>, bool[]> mazeMap)
+    public Maze(Dictionary<(int, int), bool[]> mazeMap)
     {
         _mazeMap = mazeMap;
     }
 
-    // TODO Problem 4 - ADD YOUR CODE HERE
     /// <summary>
-    /// Check to see if you can move left.  If you can, then move.  If you
-    /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
+    /// Attempt to move left in the maze.
+    /// If allowed, decrease the X position by 1.
+    /// If blocked by a wall, throw InvalidOperationException.
     /// </summary>
     public void MoveLeft()
     {
-        // FILL IN CODE
+        var currentPosition = (_currX, _currY);
+        if (!_mazeMap[currentPosition][0])
+        {
+            throw new InvalidOperationException("Can't go that way!");
+        }
+        _currX -= 1;
     }
 
     /// <summary>
-    /// Check to see if you can move right.  If you can, then move.  If you
-    /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
+    /// Attempt to move right in the maze.
+    /// If allowed, increase the X position by 1.
+    /// If blocked by a wall, throw InvalidOperationException.
     /// </summary>
     public void MoveRight()
     {
-        // FILL IN CODE
+        var currentPosition = (_currX, _currY);
+        if (!_mazeMap[currentPosition][1])
+        {
+            throw new InvalidOperationException("Can't go that way!");
+        }
+        _currX += 1;
     }
 
     /// <summary>
-    /// Check to see if you can move up.  If you can, then move.  If you
-    /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
+    /// Attempt to move up in the maze.
+    /// If allowed, decrease the Y position by 1.
+    /// If blocked by a wall, throw InvalidOperationException.
     /// </summary>
     public void MoveUp()
     {
-        // FILL IN CODE
+        var currentPosition = (_currX, _currY);
+        if (!_mazeMap[currentPosition][2])
+        {
+            throw new InvalidOperationException("Can't go that way!");
+        }
+        _currY -= 1;
     }
 
     /// <summary>
-    /// Check to see if you can move down.  If you can, then move.  If you
-    /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
+    /// Attempt to move down in the maze.
+    /// If allowed, increase the Y position by 1.
+    /// If blocked by a wall, throw InvalidOperationException.
     /// </summary>
     public void MoveDown()
     {
-        // FILL IN CODE
+        var currentPosition = (_currX, _currY);
+        if (!_mazeMap[currentPosition][3])
+        {
+            throw new InvalidOperationException("Can't go that way!");
+        }
+        _currY += 1;
     }
 
+    /// <summary>
+    /// Returns a string showing the current (x, y) position in the maze.
+    /// </summary>
     public string GetStatus()
     {
         return $"Current location (x={_currX}, y={_currY})";
--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -1,88 +1,102 @@
+using System.Diagnostics;
 using System.Text.Json;
 
+/// <summary>
+/// Class that implements problems related to sets and maps.
+/// </summary>
 public static class SetsAndMaps
 {
     /// <summary>
-    /// The words parameter contains a list of two character 
-    /// words (lower case, no duplicates). Using sets, find an O(n) 
-    /// solution for returning all symmetric pairs of words.  
-    ///
-    /// For example, if words was: [am, at, ma, if, fi], we would return :
-    ///
-    /// ["am & ma", "if & fi"]
-    ///
-    /// The order of the array does not matter, nor does the order of the specific words in each string in the array.
-    /// at would not be returned because ta is not in the list of words.
-    ///
-    /// As a special case, if the letters are the same (example: 'aa') then
-    /// it would not match anything else (remember the assumption above
-    /// that there were no duplicates) and therefore should not be returned.
+    /// Returns all symmetric word pairs using a HashSet for O(n) time complexity.
+    /// A symmetric pair is where the reverse of the word also exists in the list.
+    /// Words like "am" and "ma" form a symmetric pair, but duplicates or identical letters like "aa" are ignored.
     /// </summary>
-    /// <param name="words">An array of 2-character words (lowercase, no duplicates)</param>
     public static string[] FindPairs(string[] words)
     {
-        // TODO Problem 1 - ADD YOUR CODE HERE
-        return [];
+        var result = new List<string>();
+        var seen = new HashSet<string>();
+
+        foreach (var word in words)
+        {
+            // Skip words with same characters (e.g. "aa")
+            if (word[0] == word[1]) continue;
+
+            string reverse = new string(new char[] { word[1], word[0] });
+
+            if (seen.Contains(reverse))
+            {
+                result.Add($"{word} & {reverse}");
+            }
+
+            seen.Add(word);
+        }
+
+        return result.ToArray();
     }
 
     /// <summary>
-    /// Read a census file and summarize the degrees (education)
-    /// earned by those contained in the file.  The summary
-    /// should be stored in a dictionary where the key is the
-    /// degree earned and the value is the number of people that 
-    /// have earned that degree.  The degree information is in
-    /// the 4th column of the file.  There is no header row in the
-    /// file.
+    /// Summarizes the degrees earned by parsing the 4th column of a CSV file.
+    /// Populates a dictionary with degree name as key and occurrence count as value.
     /// </summary>
-    /// <param name="filename">The name of the file to read</param>
-    /// <returns>fixed array of divisors</returns>
     public static Dictionary<string, int> SummarizeDegrees(string filename)
     {
         var degrees = new Dictionary<string, int>();
+
         foreach (var line in File.ReadLines(filename))
         {
             var fields = line.Split(",");
-            // TODO Problem 2 - ADD YOUR CODE HERE
+
+            if (fields.Length >= 4)
+            {
+                string degree = fields[3].Trim();
+
+                if (degrees.ContainsKey(degree))
+                {
+                    degrees[degree]++;
+                }
+                else
+                {
+                    degrees[degree] = 1;
+                }
+            }
         }
 
         return degrees;
     }
 
     /// <summary>
-    /// Determine if 'word1' and 'word2' are anagrams.  An anagram
-    /// is when the same letters in a word are re-organized into a 
-    /// new word.  A dictionary is used to solve the problem.
-    /// 
-    /// Examples:
-    /// is_anagram("CAT","ACT") would return true
-    /// is_anagram("DOG","GOOD") would return false because GOOD has 2 O's
-    /// 
-    /// Important Note: When determining if two words are anagrams, you
-    /// should ignore any spaces.  You should also ignore cases.  For 
-    /// example, 'Ab' and 'Ba' should be considered anagrams
-    /// 
-    /// Reminder: You can access a letter by index in a string by 
-    /// using the [] notation.
+    /// Determines whether two words are anagrams using a dictionary of character counts.
+    /// Ignores spaces and case sensitivity.
     /// </summary>
     public static bool IsAnagram(string word1, string word2)
     {
-        // TODO Problem 3 - ADD YOUR CODE HERE
-        return false;
+        var dict1 = BuildCharFrequency(word1);
+        var dict2 = BuildCharFrequency(word2);
+
+        return dict1.Count == dict2.Count && !dict1.Except(dict2).Any();
     }
 
     /// <summary>
-    /// This function will read JSON (Javascript Object Notation) data from the 
-    /// United States Geological Service (USGS) consisting of earthquake data.
-    /// The data will include all earthquakes in the current day.
-    /// 
-    /// JSON data is organized into a dictionary. After reading the data using
-    /// the built-in HTTP client library, this function will return a list of all
-    /// earthquake locations ('place' attribute) and magnitudes ('mag' attribute).
-    /// Additional information about the format of the JSON data can be found 
-    /// at this website:  
-    /// 
-    /// https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
-    /// 
+    /// Helper method to build character frequency dictionary from a string.
+    /// Converts to lowercase and ignores spaces.
+    /// </summary>
+    private static Dictionary<char, int> BuildCharFrequency(string word)
+    {
+        var dict = new Dictionary<char, int>();
+        foreach (char c in word.ToLower())
+        {
+            if (c == ' ') continue;
+            if (dict.ContainsKey(c))
+                dict[c]++;
+            else
+                dict[c] = 1;
+        }
+        return dict;
+    }
+
+    /// <summary>
+    /// Downloads daily earthquake data from USGS and extracts location and magnitude information.
+    /// Requires FeatureCollection.cs with correct structure to match JSON response.
     /// </summary>
     public static string[] EarthquakeDailySummary()
     {
@@ -96,11 +110,20 @@ public static class SetsAndMaps
 
         var featureCollection = JsonSerializer.Deserialize<FeatureCollection>(json, options);
 
-        // TODO Problem 5:
-        // 1. Add code in FeatureCollection.cs to describe the JSON using classes and properties 
-        // on those classes so that the call to Deserialize above works properly.
-        // 2. Add code below to create a string out each place a earthquake has happened today and its magitude.
-        // 3. Return an array of these string descriptions.
-        return [];
+        var summary = new List<string>();
+
+        if (featureCollection != null && featureCollection.Features != null)
+        {
+            foreach (var feature in featureCollection.Features)
+            {
+                var props = feature.Properties;
+                if (props != null && !string.IsNullOrEmpty(props.Place) && props.Mag != null)
+                {
+                    summary.Add($"{props.Place} - Mag {props.Mag:F2}");
+                }
+            }
+        }
+
+        return summary.ToArray();
     }
 }
